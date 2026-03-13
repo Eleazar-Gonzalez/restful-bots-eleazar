d8bb743685a7c73afec3ea3e6776c71bfe1d2bab
Eleazar Gonzalez <gonzalezmartineze25@students.ecu.edu>
2026-03-12 22:39:32 -0400

    Refactor CalcController to include hello endpoint
    Removed arithmetic operations and added a hello endpoint.

--- CalcController.java
+++ CalcController.java
@@ -1,34 +1,15 @@
-package com.example.appengine.java8;
+/*
+package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalcController {
 
-    @GetMapping("/add")
-    public int add(@RequestParam int n, @RequestParam int m) {
-        return n + m;
-    }
-
-    @GetMapping("/subtract")
-    public int subtract(@RequestParam int n, @RequestParam int m) {
-        return n - m;
-    }
-
-    @GetMapping("/times")
-    public int times(@RequestParam int n, @RequestParam int m) {
-        return n * m;
-    }
-
-    @GetMapping("/div")
-    public int div(@RequestParam int n, @RequestParam int m) {
-        return n / m;
-    }
-
-    @GetMapping("/neg")
-    public int neg(@RequestParam int n) {
-        return -n;
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
     }
 }
+*/
